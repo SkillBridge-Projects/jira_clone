40c36fec9bbf00f9aaa5bc2ef27b8f30b2d51ec2
syed javeed <syedjaveed@nextgen-is.com>
2024-08-30 11:34:09 +0530

    update mail config

--- api/src/utils/mailer.ts
+++ api/src/utils/mailer.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp-mail.outlook.com',
+  host: 'smtp.hostinger.com',
   port: 587,
   secure: false,
   auth: {
